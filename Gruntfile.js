fff7229befb07a9b95c13abd062b49c3bcddcec6
Will Barton <will@willbarton.com>
2014-10-12 15:27:13 -0400

    Cleanup. Add CSS to list, replace readme.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,4 @@
 module.exports = function(grunt) {
-
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
@@ -12,7 +11,7 @@ module.exports = function(grunt) {
                 // the files to concatenate
                 src: ['bookkit/js/*.js'],
                 // the location of the resulting JS file
-                dest: 'dist/<%= pkg.name %>.js'
+                dest: 'dist/js/<%= pkg.name %>.js'
             },
         },
         uglify: {
@@ -22,19 +21,30 @@ module.exports = function(grunt) {
             },
             dist: {
                 files: {
-                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                    'dist/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
                 }
             }
         },
+        copy: {
+            main: {
+                expand: true, 
+                cwd: 'bookkit/', 
+                src: ['**'], 
+                dest: 'dist/',
+            },
+        },
+
+        clean: ["dist"]
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     grunt.registerTask('test', ['qunit']);
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('default', ['clean', 'copy', 'concat', 'uglify']);
 }
 
